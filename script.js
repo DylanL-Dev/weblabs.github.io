b06a4ed83aee3376b72ecad024a8dc3657ee58d4
Dylan LEFEBVRE <dylan.lefebvre76@live.com>
2023-11-06 13:36:37 +0100

    ajout des description pour chaque section en systeme overlay

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const overlayLinks = document.querySelectorAll(".overlay-link");
+
+  overlayLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const overlayId = link.getAttribute("data-overlay");
+      const overlay = document.getElementById(overlayId);
+      overlay.style.display = "flex";
+    });
+  });
+
+  const overlays = document.querySelectorAll(".overlay");
+
+  overlays.forEach(function (overlay) {
+    overlay.addEventListener("click", function () {
+      overlay.style.display = "none";
+    });
+  });
+});
